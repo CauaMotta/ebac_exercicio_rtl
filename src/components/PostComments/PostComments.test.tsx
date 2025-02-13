86889fb0d9b7f40ba6517633f3354cdbcc111ac6
Cauã Motta <ocauamotta@gmail.com>
2025-02-13 10:31:57 -0300

    Exercicio finalizado

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,34 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+});
+
+describe("Teste de inserção de 2 novos comentarios", () => {
+  test("Deve adicionar 2 novos comentarios", () => {
+    render(<PostComment />);
+
+    // Adiciona o primeiro comentario
+    fireEvent.change(screen.getByTestId("textarea-comentario"), {
+      target: {
+        value: "comentario 1",
+      },
     });
+    fireEvent.click(screen.getByTestId("btn-comentario"));
+
+    // Adiciona o segundo comentario
+    fireEvent.change(screen.getByTestId("textarea-comentario"), {
+      target: {
+        value: "comentario 2",
+      },
+    });
+    fireEvent.click(screen.getByTestId("btn-comentario"));
+
+    // Testa se os comentarios foram adicionados
+    expect(screen.getAllByTestId("comentario")).toHaveLength(2);
+  });
 });
